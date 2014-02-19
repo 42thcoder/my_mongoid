a14df2738756170a0e6d23772bc4ef3ef709d22a
42thcoder <42thcoder@gmail.com>
2014-02-19 19:16:51 +0800

    declare a model

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,5 +1,52 @@
 require "my_mongoid/version"
+require 'active_support/concern'
 
 module MyMongoid
-  # Your code goes here...
+
+	module Document
+		extend ActiveSupport::Concern
+
+		@@models = []
+
+		included do
+			extend ClassMethods
+
+			@@models << self
+
+			self.class_eval do
+				attr_accessor :attributes
+
+				def read_attribute(key)
+					@attributes[key]
+				end
+
+				def write_attribute(key, value)
+					@attributes[key] = value
+				end
+			end
+		end
+
+		def initialize(attributes)
+			raise ArgumentError, 'It is not a hash' unless attributes.is_a?(Hash)
+			@attributes = attributes
+
+			MyMongoid.models ||= []
+
+		end
+
+		def MyMongoid.models
+			@@models
+		end
+
+		module ClassMethods
+			def is_mongoid_model?
+				true
+			end
+
+			def field
+
+			end
+		end
+	end
+
 end
